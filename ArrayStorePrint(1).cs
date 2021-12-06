a1138155457e056fc087c3232e1402897130b11b
Rani-Biradar <95352807+Rani-Biradar@users.noreply.github.com>
2021-12-06 22:21:55 +0530

    Add files via upload

---
+++ ArrayStorePrint(1).cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayPrint
+{
+    class Program
+    {
+
+        public static void Main()
+        {
+            int i;
+            int[] arr = new int[10];
+
+            Console.Write("Enter 10 elements in array \n",n);
+
+            for (i = 0; i < 10; i++)
+            {
+                Console.Write("element-{0}:", i);
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+            Console.Write("elements in array are ");
+            for (i = 0; i < 10; i++)
+            {
+                Console.Write("{0}  ", arr[i]);
+                
+            }
+            Console.Write("\n");
+            Console.Read();
+
+        }
+    }
+}
+
+
+
+
