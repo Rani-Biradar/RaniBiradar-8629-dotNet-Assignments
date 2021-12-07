2db1835446a4c4cc9f6b255aebb96a786e483dec
Rani-Biradar <95352807+Rani-Biradar@users.noreply.github.com>
2021-12-07 22:31:43 +0530

    Add files via upload

---
+++ StringCheck.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringAssignment
+{
+    class StringCheck
+    {
+        public static void Main(string[] args)
+        {
+            string str1, str2;
+            bool m;
+
+            Console.Write("Input the string : ");
+            str1 = Console.ReadLine();
+
+            Console.Write("Input the substring to  search : ");
+            str2 = Console.ReadLine();
+
+            m = str1.Contains(str2);
+
+            if (m) 
+                Console.Write("The substring exists in the string.\n");
+            else
+                Console.Write("The substring is not exists in the string. ");
+            Console.ReadLine();
+        }
+    }
+    }
+
