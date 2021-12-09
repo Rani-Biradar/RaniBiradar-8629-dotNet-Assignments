3d5744dd640fb922131d95f8b4c788bb386adb23
Rani-Biradar <95352807+Rani-Biradar@users.noreply.github.com>
2021-12-09 13:33:56 +0530

    Add files via upload

---
+++ RecursionEvenOdd.cs
@@ -0,0 +1,42 @@
+﻿/*Write a program in C to print even or odd numbers in a given range using recursion.
+Test Data :
+Input the range to print starting from 1 : 20
+Expected Output :
+All even numbers from 1 to 20 are :
+2 4 6 8 10 12 14 16 18 20*/
+
+
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecursionAssignments
+{
+    class RecursionEvenOdd
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(" Input the range to print starting from 1 : ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\n All even numbers from 1 to {0} are : ", n);
+            EvenAndOdd(2, n);
+
+            Console.WriteLine("\n\n All odd numbers from 1 to {0} are : ", n);
+            EvenAndOdd(1, n);
+            Console.ReadLine();
+        }
+
+        static void EvenAndOdd(int Val, int n)
+        {
+            if (Val > n)
+                return;
+            Console.Write(" {0}  ", Val);
+            EvenAndOdd(Val + 2, n);
+        }
+    }
+}
