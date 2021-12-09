b2151aaca5a0abf12f274b891b4c9267ffcf154f
Rani-Biradar <95352807+Rani-Biradar@users.noreply.github.com>
2021-12-09 13:34:19 +0530

    Add files via upload

---
+++ RecursionFactorial.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecursionAssignments
+{
+    class RecursionFactorial
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write(" Input any positive number : ");
+            int n1 = Convert.ToInt32(Console.ReadLine());
+
+            int fact = Factorial(n1);
+
+            Console.WriteLine(" The factorial of {0} is : {1} ", n1, fact);
+            Console.ReadLine();
+        }
+
+        private static int  Factorial(int n1)
+        {
+            if (n1 == 0)
+            {
+                return 1;
+            }
+            return n1 * Factorial(n1 - 1);
+        }
+    }
+}
