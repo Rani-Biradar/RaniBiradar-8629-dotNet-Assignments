799295d2cee675d1e13bd942468741564d5cc38c
Rani-Biradar <95352807+Rani-Biradar@users.noreply.github.com>
2021-12-09 13:36:07 +0530

    Add files via upload

---
+++ RecursionStringPalindrome.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecursionAssignments
+{
+    class RecursionStringPalindrome
+    {
+        public static void Main(string[] args)
+        {
+            string str1;
+            bool tf;
+
+            Console.Write(" Input a string : ");
+            str1 = Console.ReadLine();
+            tf = IsPalindrome(str1);
+            if (tf == true)
+            {
+                Console.WriteLine(" The string is Palindrome");
+            }
+            else
+            {
+                Console.WriteLine(" The string is not a Palindrome");
+            }
+            Console.ReadLine();
+        }
+             public static  bool IsPalindrome(string str)
+            {
+                if (str.Length <= 1)
+                    return true;
+                else
+                {
+                    if (str[0] != str[str.Length - 1])
+                        return false;
+                    else
+                        return IsPalindrome(str.Substring(1, str.Length - 2));
+                }
+            }
+
+        }
+    }
+
+
---
+++ RecursionSum.cs
@@ -0,0 +1,39 @@
+﻿
+/*  Write a program in C# Sharp to find the sum of first n natural numbers using recursion. 
+Test Data :
+How many numbers to sum : 10
+Expected Output :
+The sum of first 10 natural numbers is : 55   */
+
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecursionAssignments
+{
+    class RecursionSum
+    {
+
+        static void Main(string[] args)
+        {
+            Console.Write(" How many numbers to sum : ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.Write(" The sum of first {0} natural numbers is : {1}", n, Sum(1, n));
+            Console.ReadLine();
+        }
+     
+
+        static int Sum(int min, int n)
+        {
+            if (n == min)
+                return n;
+            return n+ Sum(min, n- 1);
+        }
+    }
+
+}
+
